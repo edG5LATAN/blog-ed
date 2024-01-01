c02d3978327fed12bb2645379fc969e1971cf0e5
edwin castro <castromaradiaga@hotmail.com>
2024-01-01 01:04:07 -0600

    adicion de datos backend

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,8 +12,66 @@ function Home() {
           <p>
             Por que es tan complicado entrar al mundo de la programacion hoy en
             dia y mas cuando no tienes una base de esnsenanza y eres nuevo y
-            deseas aprender por tu propia cuenta con videos tutoriales y libros.{" "}
-            <br />
+            deseas aprender por tu propia cuenta con videos tutoriales y libros. 
+          </p>
+          <h3>FRONTEND</h3>
+           es la que se encarga de la creacion de paginas web en su forma diseno
+           colores todo lo que tenga que ver con su aspecto visual, y posee varios lenguajes
+            de programacion dentro de los cuales estan las mas usadas en la 
+            actualidad.
+            <br/><br/>
+            <h3>BACKEND</h3>
+             Es una parte muy importante como en frontend, ya que se encarga de todo 
+             lo que no se ve en los proyectos es todo lo relacionado con la base
+             de datos donde se guarda la infromacion donde se administra el control de 
+             usuarios seguridad web.
+            <br/><br/>
+
+            <h2>Lenguages de desarrollo FRONTEND</h2>
+            <h3>JS</h3> 
+            <img className="img1" src="img/javas.png" alt="prr" />
+            JavaScript posee varios pero en el mercado laboral
+             encontramos con dos de los mas buscados.<br/>
+            <strong>REACT</strong>:El mas utilizado y mas solicitado por empleadores es 
+            React una libreria  creado por FACEBOOK para resolver varios problemas que 
+            tenian los frameworks de sus epocas muy facil de aprender y
+            ligero donde se instalara solo que se nesecite realizar paginas web
+            aplicacions de escritorio y aplicaciones moviles.<br/>
+            <strong>ANGULAR</strong>: El framework mas intuitivo y con una mayor linea de aprendizaje
+            por su sintaxis simple, desarrollado por Google, al ser un framework completo
+            cuenta con muchas opciones y herramientas para crear aplicaciones web mas complejas.<br/> 
+            Angular al ser un framework mas complejo es mas pesado para tu pc.
+            <br/>
+            <br/>
+
+            <h3>PHP</h3>
+            <img className="img1" src="img/javas.png" alt="prr" />
+            muy completo y muy utilizado hasta la actualidad podemos
+            encontrar muchos paginas en la actualdad que estan configuradas y creadas con PHP
+            unos ejemplos basicos son los creadores de paginas en linea como WORDEXPRESS 
+            que te ayuda a crear paginas web en linea con mucha facilidad con solo el minimo
+             de conocimiento solo con hacer simples configuraciones puedes obetener tu pagina
+             web, blog...<br/>
+            <strong>Laravel</strong>: Uno de los frameworks mas usados en las paginas web
+            , hoy en dia por su facilidad de uso y su tiempo en el mercado a podido llevar a este lenguaje 
+            a poseer muchos  <br/>
+            <strong>Symfony</strong>: Un framwework muy completo para crear paginas web y muy
+            utilizado por desarrolladores de apps podemos crear aplicaciones web muy completas
+            con microservicios y paginas web simples.
+            <br/>
+            <br/>
+            <h3>PYTHON</h3>
+            <img className="img1" src="img/javas.png" alt="prr" />
+             es el lenguage creado mas reciente pero con una alta 
+            aceptacion y utilizada por la comunidad de creacion de IA inteligencia artificial
+            <br/>
+            <strong>Django</strong>: Es un framework completo que posee Python para la 
+            creacion de paginas web y de muy facil aprendizaje su simplicidad ayuda 
+            a crear paginas web complejas y robustas.<br/>
+            <strong>Flask</strong> : Un framework mas simple y menos pesado pero con las 
+            mismas caracteristicas para la creacion de paginas web pero con una mayor
+            manejo y control de componentes 
+
             <br />
             Es frustante al inicio no tener una linea marcada de como iniciar
             con tanta informacion en internet y con tantos lenguajes de
@@ -24,19 +82,21 @@ function Home() {
             Haviendo tanto lenguajes de programacion e IDE de desarrollo y no
             tener informacion o guia las personas se abruman de tanta
             informacion, pero hoy te contare como recrear tu linea de
-            aprendizaje.
+            aprendizaje utilizando JS.
             <br />
             La repuesta es mas sencilla de lo que piensas y aca te explicare
             porque y como.
-          </p>
         </div>
       </div>
 
       <div className="home-cajas">
         <div className="home-caja1">
           <h3>Que debo Estudiar y aprender primero</h3>
+
           <p>
-            Lo primero que se debe estudiar y aprender es lo mas basico
+            Aca te mostrare como crear paginas web utilizando el lenguage 
+            mas estandarizado por su uso, lo primero que se debe estudiar
+             y aprender es lo mas basico
             <strong> HTML</strong> <strong>CSS</strong> Y <strong>JS</strong>.
             <br />
             <br />
@@ -71,23 +131,25 @@ function Home() {
 
       <div className="home-cajas">
         <div className="home-caja1">
-          <h3>Que es lo que sigue</h3>
+          <h3>Cual es el siguiente paso</h3>
           <p>
             Ya una vez ya hayas manejando lo basico de HTML, CSS y JS podremos
-            pasar al siguiente nivel que seria escojer un framework y
+            pasar al siguiente nivel que seria escojer un framework de FRONTEND y
             especializarte las mas comunes son
             <br />
             <br />
             <strong>REACT</strong>: El mas utilizado y mas solicitado por empleadores es 
-            React un framework o mas bien como libreria creado por FACEBOOK para resolver 
-            varios problemas que tenian los frameworks de sus epocas muy facil de aprender y
-            ligero donde se instalara solo que se nesecite realizar 
-            muchos de los fra.
+            React una libreria  creado por FACEBOOK para resolver varios problemas que 
+            tenian los frameworks de sus epocas muy facil de aprender y
+            ligero donde se instalara solo que se nesecite realizar paginas web
+            aplicacions de escritorio y aplicaciones moviles.<br/>
+            
             <br />
             <br />
-            <strong>JS</strong> las siglas de JavaScript es un lenguaje de
-            desarrollo que hace que tu proyecto de HTML y CSS puedan llegar a
-            terner contenido dinamico controlar animaciones sonidos .
+            <strong>ANGULAR</strong>: El framework mas intuitivo y con una mayor linea de aprendizaje
+            por su sintaxis simple, desarrollado por Google, al ser un framework completo
+            cuenta con muchas opciones y herramientas para crear aplicaciones web mas complejas.<br/> 
+            Angular al ser un framework mas complejo es mas pesado para tu pc.
           </p>
         </div>
       </div>
